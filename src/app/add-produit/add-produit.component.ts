1e8f58ea2710d587feb34ca20650e25b61ec9830
khaliloz-creator <khalilamara72@gmail.com>
2020-12-11 11:40:46 +0100

    atelier3

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -5,29 +5,13 @@ import { ProduitService } from '../service/produit.service';
 @Component({
   selector: 'app-add-produit',
   templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css'],
 })
 export class AddProduitComponent implements OnInit {
-
   newProduit = new Produit();
-
-  message : string;
-
-
-  constructor(private produitService: ProduitService) { }
-  
-  ngOnInit(): void {
-
+  constructor(private produitService: ProduitService) {}
+  addProduit() {
+    this.produitService.ajouterProduit(this.newProduit);
   }
-  
-
-  addProduit(){
-  //console.log(this.newProduit);
-   this.produitService.ajouterProduit(this.newProduit);
-   this.message = "produit " + this.newProduit.nomProduit + "ajouté avec succés !";
-  }
-
- 
-
-  
-
+  ngOnInit(): void {}
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import{Produit} from '../model/produit.model';
+import { Produit } from '../model/produit.model';
 import { ProduitService } from '../service/produit.service';
 
 @Component({
   selector: 'app-produits',
-  templateUrl:'./produits.component.html'
-
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
 })
 export class ProduitsComponent implements OnInit {
+ 
+  produits : Produit[]; //un tableau de Produit
 
-  produits : Produit[]; 
-  
-  constructor(private produitService: ProduitService) { 
-    
-    this.produits= produitService.listeProduits();
+
+  constructor(private produitService: ProduitService ) {
+    this.produits = produitService.listeProduits();
+   }
+
+   supprimerProduit(p: Produit)
+   {
+      //console.log(p);
+      let conf = confirm("Etes-vous sûr ?");
+      if (conf)
+        this.produitService.supprimerProduit(p);
+   }
+
+  ngOnInit() {
   }
-
-  ngOnInit(): void{
-
-  }
-
 
 }
